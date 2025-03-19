84357e54ac5f5e0a8fc509493653a487fcd7c5a3
jikang-jeong <138367697+jikang-jeong@users.noreply.github.com>
2025-03-19 22:35:36 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import React from "react";
 import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
 import { Button, withAuthenticator } from "@aws-amplify/ui-react";
-import {
-  createStorageBrowser,
-  createAmplifyAuthAdapter,
-  elementsDefault
-} from "@aws-amplify/ui-react-storage/browser";
+// 구 버전에서는 elementsDefault가 export됩니다.
+import { createStorageBrowser, createAmplifyAuthAdapter, elementsDefault } from "@aws-amplify/ui-react-storage/browser";
 import "@aws-amplify/ui-react-storage/styles.css";
 import config from "../amplify_outputs.json";
 
@@ -15,6 +12,7 @@ Amplify.configure(config);
 
 function Example() {
   const authAdapter = createAmplifyAuthAdapter();
+  // elements 옵션에 elementsDefault를 전달하면, 구 버전에서는 파일 리스트 아이템에 삭제 체크박스 UI 등이 표시됩니다.
   const { StorageBrowser } = createStorageBrowser({
     authAdapter,
     elements: elementsDefault,
