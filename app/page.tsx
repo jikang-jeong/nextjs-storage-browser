1289f5895ddc7daf236127a369e16515af184d6f
jikang-jeong <138367697+jikang-jeong@users.noreply.github.com>
2025-03-19 22:06:09 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
-
 import { Button, withAuthenticator } from "@aws-amplify/ui-react";
 import { createStorageBrowser, createAmplifyAuthAdapter } from "@aws-amplify/ui-react-storage/browser";
+import { elementsDefault } from "./elementsDefault"; // elementsDefault.ts 파일 경로에 맞게 수정하세요.
 import "@aws-amplify/ui-react-storage/styles.css";
 import config from "../amplify_outputs.json";
 
@@ -14,13 +14,14 @@ function Example() {
   const authAdapter = createAmplifyAuthAdapter();
   const { StorageBrowser } = createStorageBrowser({
     authAdapter,
+    elements: elementsDefault,
     listPrefixes: [
       "media-readwritedelete/",
       "media-readonly/",
       "shared-folder-readwrite/",
       (identityId: string) => `protected-useronlyreadwritedelete/${identityId}/`,
-      (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`
-    ]
+      (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`,
+    ],
   });
 
   return (
