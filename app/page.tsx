484a2d42a1f05006fe52ba8be907760932c427e2
jikang-jeong <138367697+jikang-jeong@users.noreply.github.com>
2025-03-19 17:22:13 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,47 @@
 'use client';
-import React from "react";
-import { Amplify } from "aws-amplify";
-import { signOut } from "aws-amplify/auth";
 
-import { getCurrentUser } from "aws-amplify/auth";
-import { Button, withAuthenticator } from "@aws-amplify/ui-react";
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+import { signOut, getCurrentUser } from 'aws-amplify/auth';
+import { Button, withAuthenticator } from '@aws-amplify/ui-react';
 import {
   createStorageBrowser,
   createAmplifyAuthAdapter,
   elementsDefault,
-} from "@aws-amplify/ui-react-storage/browser";
-import "@aws-amplify/ui-react-storage/styles.css";
-import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
+} from '@aws-amplify/ui-react-storage/browser';
+import '@aws-amplify/ui-react-storage/styles.css';
+import '@aws-amplify/ui-react-storage/storage-browser-styles.css';
 
-import config from "../amplify_outputs.json";
+import config from '../amplify_outputs.json';
 
 Amplify.configure(config);
 
 function Example() {
+  // 디버그 로그: 현재 로그인 유저 정보 출력
+  getCurrentUser()
+    .then((user) => console.log('Current user:', user))
+    .catch((err) => console.log('No user signed in:', err));
 
-// App 시작 시 또는 Example 함수 내부에서 테스트:
-getCurrentUser()
-  .then(user => console.log("Current user:", user))
-  .catch(err => console.log("No user signed in:", err));
-
+  // StorageBrowser 생성 (정식 패키지 기준)
   const { StorageBrowser } = createStorageBrowser({
     elements: elementsDefault,
     config: createAmplifyAuthAdapter({
       options: {
         defaultPrefixes: [
-          "media-readwritedelete/",
-          "media-readonly/",
-          "shared-folder-readwrite/",
+          'media-readwritedelete/',
+          'media-readonly/',
+          'shared-folder-readwrite/',
           (identityId: string) => `protected-useronlyreadwritedelete/${identityId}/`,
           (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`,
         ],
       },
     }),
+    debug: true, // 디버그 로그 출력 활성화
   });
 
   return (
-    <>
+    <div className="p-4 space-y-4">
       <Button
-        marginBlockEnd="xl"
         size="small"
         onClick={() => {
           signOut();
@@ -51,7 +50,7 @@ getCurrentUser()
         Sign Out
       </Button>
       <StorageBrowser />
-    </>
+    </div>
   );
 }
 
