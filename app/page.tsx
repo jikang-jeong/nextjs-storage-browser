d76c344cd30f11e8e51be8e3a22b5d32e8d33b11
jikang-jeong <138367697+jikang-jeong@users.noreply.github.com>
2025-03-19 22:27:35 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import { signOut } from "aws-amplify/auth";
-
 import { Button, withAuthenticator } from "@aws-amplify/ui-react";
-import { createStorageBrowser, createAmplifyAuthAdapter } from "@aws-amplify/ui-react-storage/browser";
-import { elementsDefault } from "./elementsDefault"; // elementsDefault.tsx 파일 (JSX 포함 시 .tsx 권장)
+import {
+  createStorageBrowser,
+  createAmplifyAuthAdapter,
+  elementsDefault
+} from "@aws-amplify/ui-react-storage/browser";
 import "@aws-amplify/ui-react-storage/styles.css";
 import config from "../amplify_outputs.json";
 
@@ -13,16 +15,16 @@ Amplify.configure(config);
 
 function Example() {
   const authAdapter = createAmplifyAuthAdapter();
-  // authAdapter를 createStorageBrowser의 인자로 넘기지 않고, 대신 나머지 옵션만 전달합니다.
   const { StorageBrowser } = createStorageBrowser({
+    authAdapter,
     elements: elementsDefault,
     listPrefixes: [
       "media-readwritedelete/",
       "media-readonly/",
       "shared-folder-readwrite/",
       (identityId: string) => `protected-useronlyreadwritedelete/${identityId}/`,
-      (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`,
-    ],
+      (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`
+    ]
   });
 
   return (
@@ -30,8 +32,7 @@ function Example() {
       <Button marginBlockEnd="xl" size="small" onClick={() => signOut()}>
         Sign Out
       </Button>
-      {/* authAdapter를 StorageBrowser 컴포넌트의 prop으로 전달합니다 */}
-      <StorageBrowser authAdapter={authAdapter} />
+      <StorageBrowser />
     </>
   );
 }
